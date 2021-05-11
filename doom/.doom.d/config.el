90a486164a85c75de3c9d42ee073e5291b89aa97
Sergei Melnikov <melnikhov@gmail.com>
2021-05-11 12:39:35 +0300

    Add Doom Emacs configuration files

---
+++ doom/.doom.d/config.el
@@ -0,0 +1,110 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+(setq user-full-name "Sergey Melnikov"
+      user-mail-address "melnikhov@gmail.com"
+      doom-font (font-spec :family "JetBrains Mono" :size 16 :weight 'light)
+      doom-theme 'doom-vibrant
+      org-directory "~/Library/Mobile Documents/iCloud~com~appsonthemove~beorg/Documents/org/"
+      org-roam-directory "~/Library/Mobile Documents/iCloud~com~appsonthemove~beorg/Documents/org/"
+      +org-roam-open-buffer-on-find-file nil
+      display-line-numbers-type nil
+      shell-file-name "/bin/bash"
+      fill-column 80
+      projectile-project-search-path '("~/code/"))
+
+(setq display-time-24hr-format t)
+(display-time-mode 1)
+;; (setq display-time-day-and-date t)
+
+(setenv "PATH" "/bin:/usr/local/bin:/usr/bin:/usr/sbin/:/sbin:")
+
+(add-hook 'after-init-hook 'org-roam-mode)
+(setq org-roam-capture-templates
+        '(("l" "Lit" plain (function org-roam--capture-get-point)
+           "%?"
+           :file-name "${slug}"
+           :head "#+title: ${title}\n"
+           :unnarrowed t)
+          ("r" "Roam note" entry (file create-org-file)
+           "* #+title:\n\n\n* Ссылки\n\n←  \n#+roam_tags:"
+           :unnarrowed t)
+          ("p" "Private" plain (function org-roam-capture--get-point)
+           "%?"
+           :head "#+title:
+#+roam_tags:"
+           :unnarrowed t)))
+
+;; This function return quantity of three-symbols org files
+(defun get-files-quantity ()
+    (string-to-number
+        (shell-command-to-string "ls ~/Library/Mobile\\ Documents/iCloud~com~appsonthemove~beorg/Documents/org/???.org | wc -l")))
+
+;; This function return string line from notes-id.txt with id
+(defun get-id-from-file (filePath)
+    "Return the contents of filename's line"
+    (with-temp-buffer
+        (insert-file-contents filePath nil (- (* 4 notes-quantity) 4) (- (* 4 notes-quantity) 1))
+        (buffer-string)))
+
+(defun create-org-file ()
+    "Create an org file in ~/Library/Mobile\ Documents/iCloud~com~appsonthemove~beorg/Documents/org/"
+    (interactive)
+    (setq notes-quantity (+ 1 (get-files-quantity)))
+    (let ((name (get-id-from-file "~/Library/Mobile Documents/iCloud~com~appsonthemove~beorg/Documents/org/notes-id.txt")))
+      (expand-file-name (format "%s.org"
+                                name) "~/Library/Mobile Documents/iCloud~com~appsonthemove~beorg/Documents/org/")))
+
+(map! :leader
+      (:prefix-map ("n" . "notes")
+       (:prefix ("k" . "anki")
+        :desc "Anki mode" "m" #'anki-editor-mode
+        :desc "Insert Anki note" "i" #'anki-editor-insert-note
+        :desc "Push notes" "p" #'anki-editor-push-notes
+        :desc "Retry pushing notes" "r" #'anki-editor-retry-failure-notes))
+      (:prefix-map ("t" . "toggle")
+        :desc "Display column indicator" "c" #'display-fill-column-indicator-mode))
+
+(custom-set-variables
+ '(package-selected-packages (quote (evil-escape evil-better-visual-line org-roam))))
+
+(use-package evil-better-visual-line
+  :ensure t
+  :config
+  (evil-better-visual-line-on))
+
+;; Full screen mode
+(if (eq initial-window-system 'x)                 ; if started by emacs command or desktop file
+    (toggle-frame-maximized)
+    (toggle-frame-fullscreen))
+
+;; Auto change language to english when enter normal mode from insert mode
+(setq lang_source "com.apple.keylayout.US")                     ;set default var lang_source for issw arg
+(add-hook 'evil-insert-state-entry-hook                         ;what we do when enter insert mode
+          (lambda ()
+            (shell-command (concat "issw " lang_source))))      ;
+(add-hook 'evil-insert-state-exit-hook                          ;what we do when enter normal mode
+          (lambda ()
+            (setq lang_source (shell-command-to-string "issw"))
+            (shell-command "issw com.apple.keylayout.US")))
+(setq lang_source "com.apple.keylayout.US")                     ;set default var lang_source for issw arg
+(add-hook 'evil-replace-state-entry-hook                         ;what we do when enter insert mode
+          (lambda ()
+            (shell-command (concat "issw " lang_source))))      ;
+(add-hook 'evil-replace-state-exit-hook                          ;what we do when enter normal mode
+          (lambda ()
+            (setq lang_source (shell-command-to-string "issw"))
+            (shell-command "issw com.apple.keylayout.US")))
+
+(add-hook 'dired-mode-hook
+      (lambda ()
+        (dired-hide-details-mode)))
+
+(setq
+    org-superstar-headline-bullets-list '("⁖" "◉" "○" "✸" "✿")
+)
+
+(setq deft-directory "~/Library/Mobile Documents/iCloud~com~appsonthemove~beorg/Documents/org/"
+      deft-extensions '("org")
+      deft-recursive t
+      deft-org-mode-title-prefix nil
+      deft-time-format " %Y.%m.%d")
---
+++ doom/.doom.d/custom.el
@@ -0,0 +1,48 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#242730" "#ff665c" "#7bc275" "#FCCE7B" "#51afef" "#C57BDB" "#5cEfFF" "#bbc2cf"])
+ '(custom-safe-themes
+   '("2f1518e906a8b60fac943d02ad415f1d8b3933a5a7f75e307e6e9a26ef5bf570" "79278310dd6cacf2d2f491063c4ab8b129fee2a498e4c25912ddaa6c3c5b621e" default))
+ '(fci-rule-color "#62686E")
+ '(jdee-db-active-breakpoint-face-colors (cons "#1c1f24" "#51afef"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#1c1f24" "#7bc275"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#1c1f24" "#484854"))
+ '(objed-cursor-color "#ff665c")
+ '(package-selected-packages
+   '(org-bullets simpleclip anki-mode org-drill anki-editor omnisharp csharp-mode evil-escape evil-better-visual-line org-roam))
+ '(pdf-view-midnight-colors (cons "#bbc2cf" "#242730"))
+ '(rustic-ansi-faces
+   ["#242730" "#ff665c" "#7bc275" "#FCCE7B" "#51afef" "#C57BDB" "#5cEfFF" "#bbc2cf"])
+ '(vc-annotate-background "#242730")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#7bc275")
+    (cons 40 "#a6c677")
+    (cons 60 "#d1ca79")
+    (cons 80 "#FCCE7B")
+    (cons 100 "#f4b96e")
+    (cons 120 "#eda461")
+    (cons 140 "#e69055")
+    (cons 160 "#db8981")
+    (cons 180 "#d082ae")
+    (cons 200 "#C57BDB")
+    (cons 220 "#d874b0")
+    (cons 240 "#eb6d86")
+    (cons 260 "#ff665c")
+    (cons 280 "#d15e59")
+    (cons 300 "#a35758")
+    (cons 320 "#754f56")
+    (cons 340 "#62686E")
+    (cons 360 "#62686E")))
+ '(vc-annotate-very-old-color nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(put 'customize-variable 'disabled nil)
